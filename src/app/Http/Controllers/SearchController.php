eb81fea33a169071d8ef44b0f30e755d6d164db3
nojino <nojinonoue@yahoo.co.jp>
2023-10-09 21:08:59 +0900

    sorting update

--- src/app/Http/Controllers/SearchController.php
+++ src/app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Shop;
+use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
@@ -40,20 +41,42 @@ class SearchController extends Controller
         return view('/index',compact('shops','areas','categories'));
         }
 
+        //if($request->sorting=="avg_low"){
+        //$shops = Shop::with('kutikomis')
+        //    ->leftJoin('kutikomis', function($join) {
+        //        $join->on('shops.id', '=', 'kutikomis.shop_id')
+        //            ->whereNull('kutikomis.deleted_at');
+        //    })
+        //    ->select('shops.*')
+        //    ->selectRaw('AVG(kutikomis.score) as kutikomis_avg_score')
+        //    ->groupBy('shops.id')
+        //    ->orderByRaw('ISNULL(kutikomis_avg_score), kutikomis_avg_score ASC')
+        //    ->get();
+        //$areas=Shop::select('area')->distinct()->get();
+        //$categories=Shop::select('category')->distinct()->get();
+        //return view('/index',compact('shops','areas','categories'));
+        //}
+
         if($request->sorting=="avg_low"){
-        $shops = Shop::with('kutikomis')
-            ->leftJoin('kutikomis', function($join) {
-                $join->on('shops.id', '=', 'kutikomis.shop_id')
-                    ->whereNull('kutikomis.deleted_at');
-            })
-            ->select('shops.*')
-            ->selectRaw('AVG(kutikomis.score) as kutikomis_avg_score')
-            ->groupBy('shops.id')
-            ->orderByRaw('ISNULL(kutikomis_avg_score), kutikomis_avg_score ASC')
-            ->get();
-        $areas=Shop::select('area')->distinct()->get();
-        $categories=Shop::select('category')->distinct()->get();
-        return view('/index',compact('shops','areas','categories'));
+            $have_kutikomi = Shop::with('kutikomis')
+                ->join('kutikomis', 'shops.id', '=', 'kutikomis.shop_id')
+                ->select('shops.*')
+                ->selectRaw('AVG(kutikomis.score) as kutikomis_avg_score')
+                ->groupBy('shops.id')
+                ->orderBy('kutikomis_avg_score', 'ASC')
+                ->get();
+
+            $no_have_kutikomi = Shop::with('kutikomis')
+                ->leftJoin('kutikomis', 'shops.id', '=', 'kutikomis.shop_id')
+                ->whereNull('kutikomis.shop_id')
+                ->select('shops.*')
+                ->get();
+
+            $shops = $have_kutikomi->concat($no_have_kutikomi);
+
+            $areas=Shop::select('area')->distinct()->get();
+            $categories=Shop::select('category')->distinct()->get();
+            return view('/index',compact('shops','areas','categories'));
         }
 
 
